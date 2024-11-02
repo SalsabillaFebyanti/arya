b555c886338eb6147fe5ca38581248bd40640229
ZAZA <156441230+SalsabillaFebyanti@users.noreply.github.com>
2024-11-02 20:29:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Halo Arya!! Gua punya sesuatu'.split('')
+const text = 'Halo Sayanggg!! Aku punya sesuatu buat kamu sayangkuu'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
